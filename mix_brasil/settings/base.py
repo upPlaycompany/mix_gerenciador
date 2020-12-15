8b19de8c6f0b0463049a273319ed0310865f4053
deodatojunior <deodatojunior999@gmail.com>
2020-12-15 16:37:03 -0400

    A

--- mix_brasil/settings/base.py
+++ mix_brasil/settings/base.py
@@ -114,9 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR, 'static'
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
