33c82319c67d8181fc2d9910cfef3194cf8effa2
deodatojunior <deodatojunior999@gmail.com>
2020-12-23 14:07:29 -0400

    papokso

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -39,13 +39,13 @@ def logar(request):
         return HttpResponseRedirect("index")
     return render(request, "login.html")
 
-@login_required
+@logar()
 def index(request):
     teste = db.collection('categorias')
     docs = teste.stream()
     return render(request, 'index.html',{'lista': docs})
 
-@login_required
+@logar()
 def logout(request):
     autent.logout(request)
     return HttpResponseRedirect("logar")
