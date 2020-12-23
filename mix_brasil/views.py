fbc2eaa6dea09c4699748c86ac36d1358a807c8e
deodatojunior <deodatojunior999@gmail.com>
2020-12-23 13:18:00 -0400

    a

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -7,25 +7,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-firebaseConfig = {
-    'apiKey' : "AIzaSyBh-DC_fXWzzcHV6XYhFQ1Ya6MWG5OjH_w",
-    'authDomain' : "mix-brasil.firebaseapp.com",
-    'databaseURL': "https://mix-brasil.firebaseio.com",
-    'projectId': "mix-brasil",
-    'storageBucket': "mix-brasil.appspot.com",
-    'messagingSenderId': "132448934641",
-    'appId': "1:132448934641:web:0843ac2954464054822750",
-    'measurementId': "G-VH0XXQFXES"
-}
-
-pyrebase = pyrebase.initialize_app(firebaseConfig)
-auth = pyrebase.auth()
-
 cred = credentials.Certificate("/app/mix_brasil/credencial.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+pyrebase = pyrebase.initialize_app()
+auth = pyrebase.auth()
 
-@login_required()
+@login_required(login_url='')
 def index(request):
     teste = db.collection('categorias')
     docs = teste.stream()
@@ -47,7 +35,7 @@ def logar(request):
         return HttpResponseRedirect("index")
     return render(request, "login.html")
 
-@login_required()
+@login_required(login_url='')
 def logout(request):
     autent.logout(request)
     return HttpResponseRedirect("logar")
