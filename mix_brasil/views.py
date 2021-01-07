47743de351263d48fc86f765dfb6bd20799f846d
deodatojunior <deodatojunior999@gmail.com>
2021-01-07 14:19:25 -0400

    jajaj

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -1,11 +1,11 @@
 import pyrebase
 from django.shortcuts import render
-from google.cloud import storage
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 import psycopg2
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -18,25 +18,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 import pandas as pd
 import pprint
 
-firebaseConfig = {
-    'apiKey' : "AIzaSyBh-DC_fXWzzcHV6XYhFQ1Ya6MWG5OjH_w",
-    'authDomain' : "mix-brasil.firebaseapp.com",
-    'databaseURL': "https://mix-brasil.firebaseio.com",
-    'projectId': "mix-brasil",
-    'storageBucket': "mix-brasil.appspot.com",
-    'messagingSenderId': "132448934641",
-    'appId': "1:132448934641:web:0843ac2954464054822750",
-    'measurementId': "G-VH0XXQFXES"
-}
-
-pyrebase = pyrebase.initialize_app(firebaseConfig)
-auth = pyrebase.auth()
-
 cred = credentials.Certificate("/app/mix_brasil/credencial.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-sto = storage.Client()
-buck = sto.bucket('mix-brasil.appspot.com')
+sto = storage.bucket('mix-brasil.appspot.com')
+
 
 def logar(request):
     next = request.GET.get('next', '/index/')
@@ -202,7 +188,7 @@ def atualizar_loja_sucesso(request):
 def adicionar_imagens_loja(request, id, cod):
     if request.method == 'POST':
         img = request.POST['img']
-        arquivo = buck.blob(img)
+        arquivo = sto.blob(img)
         arquivo.upload_from_filename(img)
         url = arquivo.generate_signed_url(
             version="v0", method="GET",
