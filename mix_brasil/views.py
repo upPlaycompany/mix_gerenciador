8077130e90e62ed948fa37fea6c6eca4298e5af8
deodatojunior <deodatojunior999@gmail.com>
2021-01-07 10:42:50 -0400

    jajaj

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -69,15 +69,15 @@ def criar_loja(request, id):
             dex = False
         price = float(price)
         dados.set({
-            'name':f'{name}',
-            'descricao':f'{descricao}',
+            'name': f'{name}',
+            'descricao': f'{descricao}',
             'price': price,
             'destaque': dex,
             'promocao': "",
             'img': firestore.ArrayUnion([""])
         })
-        info = db.collection(f'categorias/{id}/lojas').where('name','==',f'{name}').stream()
-        ff = [{'id':x.id} for x in info]
+        info = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{name}').stream()
+        ff = [{'id': x.id} for x in info]
         info2 = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{name}').stream()
         gg = [x.to_dict() for x in info2]
         a = len(ff)
@@ -88,11 +88,11 @@ def criar_loja(request, id):
             if destaque == 'true':
                 des = db.collection('destaques_home').document()
                 des.set({
-                    'cid':f"{n['categoria']}",
+                    'cid': f"{n['categoria']}",
                     'cupons': firestore.ArrayUnion([""]),
                     'img': firestore.ArrayUnion([""]),
                     'lid': f"{n['id']}",
-                    'name':f"{n['name']}",
+                    'name': f"{n['name']}",
                     'ofertas': firestore.ArrayUnion([""]),
                     'ofertas_destaque': firestore.ArrayUnion([""]),
                     'price': n['price']
@@ -116,12 +116,12 @@ def categoria_listagem(request):
 @login_required
 def lojas_listagem(request, id):
     lojas = db.collection(f'categorias/{id}/lojas').stream()
-    docs = [{'id':x.id} for x in lojas]
+    docs = [{'id': x.id} for x in lojas]
     lojas2 = db.collection(f'categorias/{id}/lojas').stream()
     docs2 = [y.to_dict() for y in lojas2]
     a = len(docs)
-    dzz = [{'categoria':f'{id}'}]
-    categoria = {'categoria':f'{id}'}
+    dzz = [{'categoria': f'{id}'}]
+    categoria = {'categoria': f'{id}'}
     [docs[x].update(docs2[x]) for x in range(a)]
     [docs[x].update(categoria) for x in range(a)]
     return render(request, 'lojas_listagem.html', {'lista': docs, 'order': dzz})
@@ -131,7 +131,7 @@ def lojas_dados(request, id, nome, cod):
     dados = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{nome}').stream()
     abc = [x.to_dict() for x in dados]
     dados2 = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{nome}').stream()
-    dec = [{'id':x.id} for x in dados2]
+    dec = [{'id': x.id} for x in dados2]
     a = len(dec)
     categoria = {'categoria': f'{id}'}
     [dec[x].update(abc[x]) for x in range(a)]
@@ -152,16 +152,16 @@ def lojas_dados(request, id, nome, cod):
         formform.update(
             {
                 'name':f'{name}',
-                'descricao':f'{descricao}',
+                'descricao': f'{descricao}',
                 'price': price,
                 'destaque': des,
-                'promocao':f'{promocao}'
+                'promocao': f'{promocao}'
             }
         )
         if des == True:
-            desdes = db.collection(f'categorias/{id}/lojas').where('name','==',f'{name}').stream()
-            da = [{'id':x.id} for x in desdes]
-            dasdas = db.collection(f'categorias/{id}/lojas').where('name','==',f'{name}').stream()
+            desdes = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{name}').stream()
+            da = [{'id': x.id} for x in desdes]
+            dasdas = db.collection(f'categorias/{id}/lojas').where('name', '==', f'{name}').stream()
             dw = [x.to_dict() for x in dasdas]
             a = len(da)
             categoria = {'categoria': f'{id}'}
@@ -178,6 +178,16 @@ def lojas_dados(request, id, nome, cod):
                 'ofertas_destaque': firestore.ArrayUnion([""]),
                 'price': n['price']
             })
+        elif des == False:
+            desdes = db.collection(f'destaques_home').where('lid', '==', f'{cod}').stream()
+            da = [{'id': x.id} for x in desdes]
+            dasdas = db.collection(f'destaques_home').where('lid', '==', f'{cod}').stream()
+            dw = [x.to_dict() for x in dasdas]
+            a = len(da)
+            [da[x].update(dw[x]) for x in range(a)]
+            for n in da:
+                db.collection('destaques_home').document(f"{n['id']}").delete()
+
         return redirect('atualizar_loja_sucesso')
     return render(request,'lojas_dados.html', {'lista':dec})
 
@@ -190,7 +200,7 @@ def adicionar_imagens_loja(request, id, cod):
     if request.method == 'POST':
         img = request.POST['img']
         formform = db.collection(f'categorias/{id}/lojas').document(f'{cod}')
-        att = formform.update({
+        formform.update({
             'img': firestore.ArrayUnion([f'{img}'])
         })
         return redirect('adicionar_imagens_loja_sucesso')
