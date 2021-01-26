8f72cf6ce81b1207641e9c49a2275663af837556
deodatojunior <deodatojunior999@gmail.com>
2021-01-26 18:38:51 -0400

    ijsoiajsoi

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -122,9 +122,9 @@ def usuario_dados(request, id):
     headers = {'Authorization': 'Token token=866968b5a2faee988b72d9c44dc63d52'}
     link = requests.get(url, headers=headers, verify=False)
     cde = link.json()
-    dados = db.collection(f'users').where('name', '==', f'{nome}').stream()
+    dados = db.collection(f'users').document(f'{id}').stream()
     abc = [x.to_dict() for x in dados]
-    dados2 = db.collection(f'users').where('name', '==', f'{nome}').stream()
+    dados2 = db.collection(f'users').document(f'{id}').stream()
     dec = [{'id': x.id} for x in dados2]
     a = len(dec)
     ident = {'ident': f'{id}'}
