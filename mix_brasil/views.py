3d44cdb13066f27c83de43e3c492cb526e3512d7
deodatojunior <deodatojunior999@gmail.com>
2021-02-17 16:02:29 -0400

    oosaksok

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -26,7 +26,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 import pandas as pd
 import pprint
 
-locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+locale.setlocale(locale.LC_TIME, 'pt_BR')
 
 config = {
     'apiKey': "AIzaSyBh-DC_fXWzzcHV6XYhFQ1Ya6MWG5OjH_w",
