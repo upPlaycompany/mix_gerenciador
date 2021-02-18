329e256420ed2f0ae2a84d8f812c83fab8db820d
deodatojunior <deodatojunior999@gmail.com>
2021-02-18 11:52:12 -0400

    oksokok

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -1069,7 +1069,7 @@ def notificacao(request, token):
             ),
             topic='all',
         )
-        messaging.send(message, app=firebase_admin.App)
+        messaging.send(message)
         return HttpResponseRedirect('notificacao_sucesso', token=token)
     return render(request, 'notificacao.html', {'t': key})
 
