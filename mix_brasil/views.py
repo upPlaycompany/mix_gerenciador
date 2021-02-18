0c5fbb3f4943d46ca3d595aa98009b9bf24c3408
deodatojunior <deodatojunior999@gmail.com>
2021-02-18 12:40:34 -0400

    Okaokaosk

--- mix_brasil/views.py
+++ mix_brasil/views.py
@@ -1058,11 +1058,11 @@ def notificacao(request, token):
             messages = [
                 messaging.Message(
                 notification=messaging.Notification(titulo, mensagem),
-                token=f"{dados4[x]['token']}"
+                token=f"{dados4[0]['token']}"
                 ),
                 messaging.Message(
                 notification=messaging.Notification(titulo, mensagem),
-                token=f"{dados4[x]['token']}"
+                token=f"{dados4[0]['token']}"
                 ),
             ]
 
